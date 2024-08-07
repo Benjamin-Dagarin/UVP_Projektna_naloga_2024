e6628e7c7e9be76a1b63ef5ede929c334bcb4eac
Benjamin Dagarin <bd4300@student.uni-lj.si>
2024-08-07 12:02:36 +0200

    Popravil komentar v uporabniski_posrednik.py

--- uporabniski_posrednik.py
+++ uporabniski_posrednik.py
@@ -1,7 +1,7 @@
 # uporabniski_posrednik.py
 
 ##############################################################################
-# Definiranje uporabniškega posrednika za dostop do spletnih strani
+# Definiranje uporabniškega posrednika za dostop do strežnika
 
 up_posrednik = ('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36' +
                  ' (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36'
